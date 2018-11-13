076660e6420fda05e51027427c01e411c47befa3
Adam Soffer <adam@soffer.space>
2018-11-12 20:40:51 -0500

    Add 404

--- pages/page.js
+++ pages/page.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 import Error from 'next/error'
 import Router, { withRouter } from 'next/router'
 import ErrorMessage from '../components/ErrorMessage'
+import { throw404 } from '../lib/helpers'
 import getComponent from '../lib/getComponent'
 
 export const allPagesQuery = gql`
@@ -44,11 +45,7 @@ export default withRouter(({ statusCode, router: { query } }) => {
       {({ loading, error, data }) => {
         if (error) return <ErrorMessage message="Error loading page." />
         if (loading) return <div>Loading</div>
-        if (data.allPages.edges.length === 0) {
-          const e = new Error('Response not found')
-          e.code = 'ENOENT' // Triggers a 404
-          throw e
-        }
+        if (data.allPages.edges.length === 0) return throw404()
         let components = data.allPages.edges[0].node.body
         return <div>{components.map(component => getComponent(component))}</div>
       }}
