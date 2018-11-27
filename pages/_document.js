53c0a0aba75025a0718f139489cc24d52d828267
Adam Soffer <adam@soffer.space>
2018-11-26 21:31:27 -0500

    fix font-face

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -16,5 +16,98 @@ export default class MyDocument extends Document {
       ...initialProps,
       styles: [...initialProps.styles, ...sheet.getStyleElement()]
     }
+  }
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-thin-webfont.woff2') format('woff2'),
+                url('/static/fonts/gilroy-thin-webfont.woff') format('woff');
+              font-weight: 100;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-light-webfont.woff2') format('woff2'),
+                url('gilroy-light-webfont.woff') format('woff');
+              font-weight: 200;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-medium-webfont.woff2')
+                  format('woff2'),
+                url('/static/fonts/gilroy-medium-webfont.woff') format('woff');
+              font-weight: 500;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-semibold-webfont.woff2')
+                  format('woff2'),
+                url('/static/fonts/gilroy-semibold-webfont.woff') format('woff');
+              font-weight: 600;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-semibolditalic-webfont.woff2')
+                  format('woff2'),
+                url('/static/fonts/gilroy-semibolditalic-webfont.woff')
+                  format('woff');
+              font-weight: 600;
+              font-style: italic;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-bold-webfont.woff2') format('woff2'),
+                url('/static/fonts/gilroy-bold-webfont.woff') format('woff');
+              font-weight: 700;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-extrabold_3-webfont.woff2')
+                  format('woff2'),
+                url('/static/fonts/gilroy-extrabold_3-webfont.woff')
+                  format('woff');
+              font-weight: 800;
+              font-style: normal;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-blackitalic-webfont.woff2')
+                  format('woff2'),
+                url('/static/fonts/gilroy-blackitalic-webfont.woff')
+                  format('woff');
+              font-weight: 900;
+              font-style: italic;
+            }
+          
+            @font-face {
+              font-family: 'gilroy';
+              src: url('/static/fonts/gilroy-black-webfont.woff2') format('woff2'),
+                url('/static/fonts/gilroy-black-webfont.woff') format('woff');
+              font-weight: 900;
+              font-style: normal;
+            }
+          `}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
   }
 }
