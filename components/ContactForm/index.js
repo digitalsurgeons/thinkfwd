1de0eae55c23e2b69260ba2719e3ec521941b713
Adam Soffer <adam@soffer.space>
2018-12-07 17:44:04 -0500

    Style select; Add react-hubspot

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -1,8 +1,21 @@
 import Select from 'react-select'
-import axios from 'axios'
-import React, { useState, useEffect, useRef } from 'react'
-import { Root, Form, FieldRow, Field, Label, Input, Textarea } from './styles'
+import {
+  Wrapper,
+  Header,
+  Headline,
+  Title,
+  Form,
+  FieldRow,
+  Field,
+  Label,
+  Input,
+  Textarea,
+  customSelectStyles
+} from './styles'
+import { fonts, colors } from '../../lib/settings'
 import Button from '../Button'
+import { useForm } from 'react-hubspot'
+import { Container } from '../../lib/helpers'
 
 const options = [
   {
@@ -24,99 +37,53 @@ const options = [
   { value: 'I have a press/media query.', label: 'I have a press/media query.' }
 ]
 
-const useHubspotApi = ({ portalId, formId }) => {
-  const [data, setData] = useState()
-  const [url, setUrl] = useState(
-    `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formId}`
-  )
-  const [form, setForm] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-
-  const fetchData = async () => {
-    setIsError(false)
-    setIsLoading(true)
-    try {
-      const formData = new FormData(form)
-      const data = {
-        fields: []
-      }
-      for (var pair of formData.entries()) {
-        data.fields.push({ name: pair[0], value: pair[1] })
-      }
-      const result = await axios({
-        method: 'post',
-        url,
-        data,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      setData(result)
-      setForm(false)
-    } catch (e) {
-      setIsError(true)
-      setForm(false)
-    }
-
-    setIsLoading(false)
-  }
-
-  useEffect(
-    () => {
-      if (form) {
-        fetchData()
-      }
-    },
-    [form]
-  )
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    setForm(e.target)
-  }
-
-  return { data, isLoading, isError, handleSubmit }
-}
-
 export default () => {
-  const { data, isLoading, isError, handleSubmit } = useHubspotApi({
+  const { data, isLoading, isError, handleSubmit } = useForm({
     portalId: '5120491',
     formId: 'e286d489-7558-49cb-9f14-5dc4466d90b4'
   })
 
   return (
-    <Root>
-      <Form onSubmit={e => handleSubmit(e)}>
-        <Field>
-          <Label>I want to know more about...</Label>
-          <Select name="subject" options={options} />
-        </Field>
-        <Field>
-          <Label>Hi, my name is</Label>
-          <FieldRow>
-            <Input name="firstname" type="text" placeholder="first name" />
-            <Input name="lastname" type="text" placeholder="last name" />
-          </FieldRow>
-        </Field>
-        <Field>
-          <Label>My email is</Label>
-          <Input name="email" type="text" placeholder="your email" />
-        </Field>
-        <Field>
-          <Label>How can we help you?</Label>
-          <Input
-            name="message"
-            rows="1"
-            as="textarea"
-            placeholder="Ex. I need to ship products and features faster then my team is currently able to. "
-            type="text"
-          />
-        </Field>
-        <Button secondary type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Root>
+    <Container>
+      <Wrapper>
+        <Header>
+          <Title>// Contact</Title>
+          <Headline>Got a question? We love questions!</Headline>
+        </Header>
+        <Form onSubmit={handleSubmit}>
+          <Field>
+            <Label>I want to know more about...</Label>
+            <Select
+              styles={customSelectStyles}
+              name="subject"
+              options={options}
+            />
+          </Field>
+          <Field>
+            <Label>Hi, my name is</Label>
+            <FieldRow>
+              <Input name="firstname" type="text" placeholder="first name" />
+              <Input name="lastname" type="text" placeholder="last name" />
+            </FieldRow>
+          </Field>
+          <Field>
+            <Label>My email is</Label>
+            <Input name="email" type="text" placeholder="your email" />
+          </Field>
+          <Field>
+            <Label>How can we help you?</Label>
+            <Input
+              name="message"
+              as="textarea"
+              placeholder="Ex. I need to ship products and features faster then my team is currently able to. "
+              type="text"
+            />
+          </Field>
+          <Button secondary type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Wrapper>
+    </Container>
   )
 }
--- components/ContactForm/styles.js
+++ components/ContactForm/styles.js
@@ -1,11 +1,46 @@
 import styled from 'styled-components'
 import { colors, fonts } from '../../lib/settings'
+import { mq } from '../../lib/helpers'
 
-export const Root = styled.div({
-  padding: '0 20px'
+export const Wrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '100px 0'
 })
 
-export const Form = styled.form({})
+export const Header = styled.header({
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: 575
+})
+
+export const Title = styled.h2({
+  fontSize: '16px',
+  color: colors.pink,
+  fontWeight: 600,
+  marginBottom: '16px',
+  fontFamily: fonts.secondary,
+  [mq[2]]: {
+    fontSize: '24px'
+  }
+})
+
+export const Headline = styled.h2({
+  fontSize: '24px',
+  lineHeight: '40px',
+  color: '#000000',
+  fontWeight: 600,
+  fontFamily: fonts.secondary,
+  [mq[2]]: {
+    fontSize: '64px',
+    lineHeight: '80px'
+  }
+})
+
+export const Form = styled.form({
+  maxWidth: 415,
+  width: '100%'
+})
 
 export const Field = styled.div({
   display: 'flex',
@@ -27,7 +62,7 @@ export const Input = styled.input({
   color: 'rgba(0, 0, 0, 0.6)',
   fontSize: 20,
   lineHeight: '28px',
-  paddingBottom: 5
+  paddingBottom: 10
 })
 
 export const FieldRow = styled.div({
@@ -39,3 +74,53 @@ export const FieldRow = styled.div({
     }
   }
 })
+
+export const customSelectStyles = {
+  placeholder: provided => ({
+    ...provided,
+    fontFamily: fonts.primary,
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: 20,
+    lineHeight: '28px',
+    paddingBottom: 10
+  }),
+  indicatorSeparator: (provided, { isSelected, isFocused }) => ({
+    display: 'none'
+  }),
+  option: (provided, { isSelected, isFocused }) => ({
+    ...provided,
+    fontFamily: fonts.primary,
+    color: isSelected ? '#fff' : 'rgba(0, 0, 0, 0.6)',
+    fontSize: 20,
+    lineHeight: '28px',
+    paddingBottom: 10,
+    backgroundColor: isSelected
+      ? 'rgba(25, 161, 219, 1)'
+      : isFocused
+      ? 'rgba(25, 161, 219, .1)'
+      : '#fff'
+  }),
+  singleValue: provided => ({
+    ...provided,
+    fontFamily: fonts.primary,
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: 20,
+    lineHeight: '28px',
+    paddingBottom: 10
+  }),
+  control: provided => ({
+    ...provided,
+    fontFamily: fonts.primary,
+    borderTop: 0,
+    borderLeft: 0,
+    borderRight: 0,
+    borderRadius: 0,
+    borderBottom: '2px solid #616161',
+    ':hover': {
+      borderBottom: '2px solid #616161'
+    },
+    ':active': {
+      boxShadow: 0
+    }
+  })
+}
