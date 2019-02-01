713589627ac9425bbb93df0bd02d3c6d187e010e
Adam Soffer <ads1018@gmail.com>
2019-02-01 14:34:17 -0500

    Fix 500 error

--- components/ToolkitArticle/index.js
+++ components/ToolkitArticle/index.js
@@ -1,27 +1,110 @@
+import { useRef, useState, useEffect } from 'react'
 import { RichText } from 'prismic-reactjs'
+import Modal from 'react-responsive-modal'
+import { useForm } from 'react-hubspot'
+import Fade from 'react-reveal/Fade'
 import { Container, linkResolver } from '../../lib/helpers'
-import { Aside, Main, Wrapper } from './styles'
+import {
+  Aside,
+  Main,
+  Wrapper,
+  Form,
+  FieldRow,
+  Field,
+  Label,
+  Input,
+  ButtonContainer
+} from './styles'
 import Button from '../Button'
+import { colors } from '../../lib/settings'
 
-export default ({ aside, main, downloadLink, downloadLinkText }) => (
-  <Container>
-    <Wrapper>
-      {aside && (
-        <Aside>
-          <div>{RichText.render(aside, linkResolver)}</div>
+export default ({ aside, main, downloadLink, downloadLinkText }) => {
+  const formEl = useRef(null)
+  const { data, isLoading, isError, handleSubmit } = useForm({
+    portalId: '521132',
+    formId: 'af89a86e-3c63-4999-a320-8263594d19c9'
+  })
+
+  const [submitted, setSubmitted] = useState(false)
+
+  useEffect(() => {
+    if (data) {
+      setSubmitted(true)
+      const submission = JSON.parse(data.config.data)
+      const email = submission.fields.find(o => o.name === 'email').value
+      window.ga('send', 'event', 'Submissions', 'submit', email)
+      formEl.current.reset()
+    }
+  }, [data])
+
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Container>
+      <Wrapper>
+        {aside && (
+          <Aside>
+            <div>{RichText.render(aside, linkResolver)}</div>
+            {downloadLink && (
+              <Button
+                as="div"
+                style={{ width: '100%' }}
+                onClick={() => setOpen(true)}>
+                {downloadLinkText ? downloadLinkText[0].text : 'Download'}
+              </Button>
+            )}
+          </Aside>
+        )}
+        {main && <Main>{RichText.render(main, linkResolver)}</Main>}
+      </Wrapper>
+
+      <Modal open={open} onClose={() => setOpen(false)} center>
+        <Form ref={formEl} onSubmit={handleSubmit}>
+          <Field>
+            <Label>Hi, my name is</Label>
+            <FieldRow>
+              <Input name="firstname" type="text" placeholder="first name" />
+              <Input name="lastname" type="text" placeholder="last name" />
+            </FieldRow>
+          </Field>
+          <Field>
+            <Label>My email is</Label>
+            <Input
+              required
+              name="email"
+              type="email"
+              placeholder="your email"
+            />
+          </Field>
           {downloadLink && (
-            <Button
-              as="a"
-              target="__blank"
-              rel="noopener noreferrer"
-              style={{ width: '100%' }}
-              href={downloadLink.url}>
-              {downloadLinkText ? downloadLinkText[0].text : 'Download'}
-            </Button>
+            <ButtonContainer>
+              <Button variant="secondary" type="submit">
+                <span>
+                  {downloadLinkText ? downloadLinkText[0].text : 'Download'}
+                </span>
+              </Button>
+              <Fade in={submitted}>
+                <div
+                  style={{
+                    marginLeft: '16px',
+                    fontSize: '16px',
+                    lineHeight: '24px',
+                    flex: 1
+                  }}>
+                  <a
+                    style={{ fontWeight: 600, color: colors.pink }}
+                    href={downloadLink.url}
+                    target="__blank">
+                    {' '}
+                    Click here
+                  </a>{' '}
+                  to download.
+                </div>
+              </Fade>
+            </ButtonContainer>
           )}
-        </Aside>
-      )}
-      {main && <Main>{RichText.render(main, linkResolver)}</Main>}
-    </Wrapper>
-  </Container>
-)
+        </Form>
+      </Modal>
+    </Container>
+  )
+}
--- components/ToolkitArticle/styles.js
+++ components/ToolkitArticle/styles.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { fonts, breakpoints } from '../../lib/settings'
+import { colors, fonts, breakpoints } from '../../lib/settings'
 import { mq } from '../../lib/helpers'
 
 export const Wrapper = styled.div({
@@ -74,4 +74,44 @@ export const Main = styled.div({
   [mq[2]]: {
     width: '66.666%'
   }
+})
+
+export const Form = styled.form({})
+
+export const Field = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  marginBottom: 40
+})
+
+export const Label = styled.label({
+  fontFamily: fonts.secondary,
+  fontSize: 24,
+  color: colors.black,
+  lineHeight: '36px',
+  marginBottom: 15
+})
+
+export const Input = styled.input({
+  border: 0,
+  borderBottom: '2px solid #616161',
+  color: 'rgba(0, 0, 0, 0.6)',
+  fontSize: 20,
+  lineHeight: '28px',
+  paddingBottom: 10
+})
+
+export const FieldRow = styled.div({
+  display: 'flex',
+  input: {
+    width: '50%',
+    ':first-of-type': {
+      marginRight: 20
+    }
+  }
+})
+
+export const ButtonContainer = styled.div({
+  display: 'flex',
+  alignItems: 'center'
 })
