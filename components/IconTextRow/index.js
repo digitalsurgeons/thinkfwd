cd1dcb2ec15ddb14a3e3fad2e956fedda797d411
Adam Soffer <adam@soffer.dev>
2019-05-24 10:04:24 -0400

    Complete toolkit grid category component

--- components/IconTextRow/index.js
+++ components/IconTextRow/index.js
@@ -1,21 +1,25 @@
 import { Container } from '../../lib/helpers'
-import { Row, Box, Title, Copy } from './styles'
+import { Root, Row, Box, Title, Copy } from './styles'
 
 export default ({ fields }) => {
   return (
-    <Container>
-      <Row>
-        {fields.map((field, i) => {
-          const { icon, title, copy, color } = field
-          return (
-            <Box key={i}>
-              <img src={icon.url} />
-              <Title color={color}>{title[0].text}</Title>
-              <Copy>{copy[0].text}</Copy>
-            </Box>
-          )
-        })}
-      </Row>
-    </Container>
+    <Root>
+      <Container>
+        <Row>
+          {fields.map((field, i) => {
+            const {
+              toolkit_category: { icon, title, description, color }
+            } = field
+            return (
+              <Box key={i}>
+                <img alt={icon.alt} src={icon.url} />
+                <Title color={color}>{title[0].text}</Title>
+                {description && <Copy>{description[0].text}</Copy>}
+              </Box>
+            )
+          })}
+        </Row>
+      </Container>
+    </Root>
   )
 }
