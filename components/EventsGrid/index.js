13c31568b92f6c723f03ff9ccf949f40474ca21e
Alex Trost <alexrtrost@gmail.com>
2020-02-24 13:53:10 -0500

    fix: Hardcode next event

--- components/EventsGrid/index.js
+++ components/EventsGrid/index.js
@@ -9,6 +9,22 @@ export default ({ fields }) => {
     <Root>
       <Container>
         <Wrapper>
+        <Link
+                route="https://www.eventbrite.com/e/codefwd-animation-tickets-93111491979"
+                passHref>
+                <StyledLink target="_blank" rel="noopener noreferrer">
+                  <Card
+                    opaque={false}
+                    imageTitle={
+                      null
+                    }
+                    image="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F91295493%2F286199106473%2F1%2Foriginal.20200206-205207?auto=format%2Ccompress&q=75&sharp=10&s=7844376306e3b7a9f604f977096a317c"
+                    title="Code:fwd - Animation"
+                    date="2020-02-25T18:00:00"
+                    description={`New Haven, CT`}
+                  />
+                </StyledLink>
+              </Link>
           {fields.map(({ event, coverage_link }, i) => {
             const isPast = moment(event.start.local).isBefore(Date.now())
             return (
